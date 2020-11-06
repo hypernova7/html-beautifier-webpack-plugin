f8b425497febb0dd43dc1705fa992483c37c27b3
hypernova7 <23711001+hypernova7@users.noreply.github.com>
2020-11-06 03:20:02 -0600

    Update html-beautify-webpack-plugin for html-webpack-plugin@4

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-const webpack = require('webpack')
-const _ = require('lodash')
 const chalk = require('chalk')
 const assert = require('assert')
+const webpack = require('webpack/package')
 const beautify = require('js-beautify').html
-const webpackLatest = !_.isNil(webpack.version) && /.*4(\.\d+){0,2}/gi.test(webpack.version)
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackLatest = (webpack.version !== null || webpack.version !== void 0) && /.*4(\.\d+){0,2}/gi.test(webpack.version)
 
-function HtmlBeautifyPlugin ({ config = {}, replace } = { config: {}, replace: [] }) {
+function HtmlBeautifyPlugin ({ config = {}, replace = [] }) {
 
-	assert(config && typeof config === 'object' , chalk.red('Beautify config should be an object.'))
+  assert(config && typeof config === 'object' , chalk.red('Beautify config should be an object.'))
 
-	this.options = {
-		config: _.merge({
-			indent_size: 4,
-			indent_with_tabs: false,
-			html: {
-				end_with_newline: true,
-				indent_inner_html: true,
-				preserve_newlines: true,
-			}
-		}, config),
-		replace: replace || []
-	}
+  this.options = {
+    config: {
+      indent_size: 4,
+      indent_with_tabs: false,
+      html: {
+        end_with_newline: true,
+        indent_inner_html: true,
+        preserve_newlines: true,
+      },
+      ...config,
+    },
+    replace
+  }
 }
 
 function htmlPluginDataFunction (htmlPluginData, callback, _this) {
-    htmlPluginData.html = beautify(_.reduce(_this.options.replace, (res, item) => {
-        if(typeof item === 'string' || item instanceof RegExp)
-            return res.replace(item instanceof RegExp ? item : new RegExp(item, 'gi'), '')
-        else
-            return res.replace(item.test instanceof RegExp ? item.test : new RegExp(item.test, 'gi'), item.with || '')
-    }, htmlPluginData.html), _this.options.config)
+  htmlPluginData.html = beautify(_this.options.replace.reduce((res, item) => {
+    if (typeof item === 'string' || item instanceof RegExp)
+      return res.replace(item instanceof RegExp ? item : new RegExp(item, 'gi'), '')
+    else
+      return res.replace(item.test instanceof RegExp ? item.test : new RegExp(item.test, 'gi'), item.with || '')
+  }, htmlPluginData.html), _this.options.config)
 
-    callback(null, htmlPluginData)
+  callback(null, htmlPluginData)
 }
 
 HtmlBeautifyPlugin.prototype.apply = function (compiler) {
-    if (!webpackLatest) {
-        compiler.plugin('compilation', compilation =>
-            compilation.plugin('html-webpack-plugin-after-html-processing', (htmlPluginData, callback) => {
-                htmlPluginDataFunction(htmlPluginData, callback, this)
-            })
-        )
-    } else {
-        compiler.hooks.compilation.tap('HtmlBeautifyPlugin', compilation =>
-            compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync('HtmlBeautifyPlugin', (htmlPluginData, callback) => {
-                htmlPluginDataFunction(htmlPluginData, callback, this)
-            })
-        )
-    }
+  if (!webpackLatest) {
+    compiler.plugin('compilation',
+      compilation => compilation.plugin('html-webpack-plugin-after-html-processing',
+        (htmlPluginData, callback) => {
+          htmlPluginDataFunction(htmlPluginData, callback, this)
+        })
+      )
+  } else {
+    compiler.hooks.compilation.tap('HtmlBeautifyPlugin',
+      (compilation) => HtmlWebpackPlugin.getHooks
+      ? HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('HtmlBeautifyPlugin',
+        (htmlPluginData, callback) => {
+          htmlPluginDataFunction(htmlPluginData, callback, this);
+        })
+      : compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync('HtmlBeautifyPlugin',
+        (htmlPluginData, callback) => {
+          htmlPluginDataFunction(htmlPluginData, callback, this);
+        })
+    )
+  }
 }
 
 module.exports = HtmlBeautifyPlugin
